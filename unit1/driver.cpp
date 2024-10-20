20fa6daac6d84a22dee5f0cf73da406a851c1f6d
zinomay <maya.bernal34@gmail.com>
2024-10-20 00:39:09 -0400

    Add Initial driver for Assignment1, and source code from textbook for Assignment 2

--- unit1/driver.cpp
+++ unit1/driver.cpp
@@ -8,24 +8,41 @@
 ***********************************/
 
 // Given code in assignment 
-template <TypeName Comparable>
+/*template <TypeName Comparable>
 int binarySearch( const vector<Comparable> & a, const Comparable & x){
   int low = 0, high = a.size()-1;
   while(low <= high) {
     int mid = (low + high)/2;
-    if( a[mid] < x)
+    
+    if( a[mid] == x ){
+      return mid; 
+    }
+    if( a[mid] < x){
       low = mid+1;
-    else if ( a[mid] > x);
+    } else {
       high = mid-1;
-    else
-      return mid;
+    }
   }
-}
+}*/
 
 int main() { 
   
   /* what is the count of comparisons of binary_search given an input array of size N for the original version?
      what is the count of comparisons of binary_serch given an input array of size N for the updated and improved version?
   */
+  int low = 0;
+  int high = a.size()-1;
+  int mid = (low + high)/2;
+  while(low <= high) {
+    mid = (low + high);
+    if( a[mid] < x){
+      low = mid+1;
+    } else {
+      high = mid;
+    }
+  }
+  if (a[mid] == x){
+    return mid; 
+  }
   return 0;
 }
