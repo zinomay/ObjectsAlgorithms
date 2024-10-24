e8faa57257f556bdc4cf9855017372cdde75e7c7
zinomay <maya.bernal34@gmail.com>
2024-10-23 22:37:54 -0400

    Add Outline for Driver of unit 2 assignment1

---
+++ unit2/driver.py
@@ -0,0 +1,19 @@
+# Unit 2 Assignment 1
+# recursive and iterative version
+# Input: N, an integer greater than or equal to 0
+# Output: a string representation of N in binary, 
+#           and the number of 1's in the binary representation of N.
+
+
+def binaryNumOfOnesRec(N):
+
+def binaryNumOfOnesIter(N):
+
+
+if __name__ == "__main__":
+    # Test the following positive integers
+    test_integers = [0,1,17,123,2048, 2**31, ((2**31)-1)]
+
+    # What is the runtime complexity of binary recursion
+    # What is the runtime complexity of binary iteration
+     
