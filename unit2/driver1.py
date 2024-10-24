abe707419474cafa27151e09dde2b2cc940e6822
zinomay <maya.bernal34@gmail.com>
2024-10-23 22:47:56 -0400

    Add Outline for Driver of Unit 2 Assignment 2

---
+++ unit2/driver1.py
@@ -0,0 +1,16 @@
+# Author: Markus Bernal
+# Date: 10/23/2024
+# Unit 2 Assignment 2
+# Majority Element
+
+def findMajority( arr ):
+    #
+
+if __name__ == "__main__":
+    print('Unit 2 Assignment 2: Majority Element')
+
+    # 1) How does the recursion terminate
+    # 2) How is the case where N is odd Handled?
+    # 3) What is the runtime complexity of the algorithm in terms of Big-Oh? 
+    # 4) how can we avoid using an extra array, B
+    # 5) write a program to compute the majority element and test it accordingly
