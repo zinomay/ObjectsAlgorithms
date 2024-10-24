a1f09e2c1b0cc78528060de7bb43d2d432ecdcfc
zinomay <maya.bernal34@gmail.com>
2024-10-23 23:11:41 -0400

    Add Initial Driver outlines for Unit 3 Assignments 1 and 2

---
+++ unit3/driver0.py
@@ -0,0 +1,17 @@
+# Author: Markus Bernal
+# Date: 10/23/2024
+# Unit 3 Assignment 1
+# Heap Insertion Analysis
+
+# Program must use array-based version of the min-heap implementation
+
+# a. Insert them into a heap one by one (percolate up)
+# b. build a heap in linear time (percolate down)
+
+if __name__=="__main__":
+    # Using two different approaches to build a heap
+    # insert integers 1 to 5000 into a heap in the following orders:
+
+    # 1. Sorted
+    # 2. Reverse-sorted
+    # 3. Random
---
+++ unit3/driver1.py
@@ -0,0 +1,18 @@
+# Author: Markus Bernal
+# Date: 10/23/2024
+# Unit 3 Assignment 2
+# Sorting Comparisons
+
+# Compare sorting algorithms: heap sort, insertion sort, merge sort, and quick sort
+
+if __name__=="__main__":
+    # sort an array of 5000 randomly generated integers using each of the algorithms and compute runtime
+    # Run each sort on the array at least 5 different times. Ensure teh same array is sorted for consistency
+
+    # Heap sort
+
+    # insertion sort
+
+    # merge sort
+
+    # quick sort
