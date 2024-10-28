f276cad20f8413c2610ab51617a64b929d394144
zinomay <maya.bernal34@gmail.com>
2024-10-28 11:26:31 -0400

    Add textbook provided file with outline for new functions and Add driver

---
+++ unit4/driver0.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************************
+* Unit 4 Programming Assignment 1
+* Collisions
+* Author: Markus Bernal
+* Date: 10/28/2024
+* 
+* Description: array based hash table using linear probing, quadratic probing, and 
+*              double hashing techniques
+* 
+* Input: 
+* 
+* Output: 
+*****************************************************************************************/
+
+
+
+int main(){
+    // Assume values are of unsigned long long int  
+
+
+    // All three hash techniques use h1(k) = kmode M as initial hash
+    // quadratic probing the ith value (h1(k) +i^2) mod M
+    // double hashing [h1(k) + i*h2(k) ]mod M  h2(k) = 1+(kmode(m-1)), M size of hash table
+    return 0;
+}
